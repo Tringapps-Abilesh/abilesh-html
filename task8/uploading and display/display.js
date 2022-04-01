9255e25551315c4a537cf9f8b57d3e673871e8af
Tringapps-Abilesh <96728976+Tringapps-Abilesh@users.noreply.github.com>
2022-04-01 18:12:50 +0530

    Add files via upload

---
+++ task8/uploading and display/display.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+  const image = localStorage.getItem('IMAGE')
+  const json = localStorage.getItem('JSON')
+
+  var uploadedImage = document.querySelector('#uploadedImage')
+  var jsonOutput = document.querySelector('#jsonOutput')
+
+  uploadedImage.src = image
+
+  var jsonText = ''
+  var jsonObject = JSON.parse(json)
+  for (var key in jsonObject) {
+    jsonText = jsonText + key + ': ' + jsonObject[key] + '<br>'
+  }
+  jsonOutput.innerHTML = jsonText
+})
