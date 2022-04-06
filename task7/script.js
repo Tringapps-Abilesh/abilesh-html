b66b3045040cd5ef731df28824e754a4c83d03a9
Tringapps-Abilesh <96728976+Tringapps-Abilesh@users.noreply.github.com>
2022-04-06 16:40:10 +0530

    Update script.js

--- task7/script.js
+++ task7/script.js
@@ -1,7 +1,7 @@
 function searchFun(){
     var key=document.querySelector("#searchBox").value.toUpperCase();
     var myItems=document.querySelectorAll(".items");
-    for(let i=0;i<myItems.length;i++){
+    for(let value of myItems){
       var inItems=myItems[i].querySelector("a").innerText.toUpperCase();
       if(inItems.indexOf(key)>-1){
         myItems[i].style.display=""
