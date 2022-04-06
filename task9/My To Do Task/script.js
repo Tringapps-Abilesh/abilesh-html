4d52ac5c9e86175981cd918f1b20429af8e63fc2
Tringapps-Abilesh <96728976+Tringapps-Abilesh@users.noreply.github.com>
2022-04-06 17:00:47 +0530

    Update script.js

--- task9/My To Do Task/script.js
+++ task9/My To Do Task/script.js
@@ -9,7 +9,6 @@ for (i = 0; i < myNodelist.length; i++) {
 }
 
 var close = document.getElementsByClassName('close')
-var i
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function () {
     var div = this.parentElement
@@ -38,9 +37,6 @@ function newElement() {
     document.getElementById('myUL').appendChild(li)
   }
   document.getElementById('myInput').value = ''
-
-  var span = document.createElement('SPAN')
-  var txt = document.createTextNode('\u00D7')
   span.className = 'close'
   span.appendChild(txt)
   li.appendChild(span)
